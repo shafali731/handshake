632297d7cb206cc4eae95791eccbaf48924e5a85
Shafalil Gupta <sgupta@stuy.edu>
2019-01-01 23:19:06 -0500

    fix

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,44 +11,32 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  //remove("WKP");
-  printf("Creating well known pipe\n");
-    int pipe_f;
-    if ((pipe_f = mkfifo("WKP", 0644)) == -1) {
-    printf("Error %d: %s\n", errno, strerror(errno));
-  }
-  printf("Connecting to WKP\n");
-    int wkp_fd;
-    if(wkp_fd = open("WKP", O_RDONLY) == -1){
-      printf("Error %d: %s\n", errno, strerror(errno));
-    }
-  printf("Connected to WKP\n");
+  printf("making WKP pipe\n");
+   mkfifo("to_server",0644);
 
-  printf("Waiting for client to respond\n");
+   printf("Connecting to WKP\n");
+   printf("Waiting for client to respond\n");
 
-  char msg[BUFFER_SIZE];
-  read(wkp_fd, msg, BUFFER_SIZE);
-  //printf("Client said: %s", msg);
+   int wkp_fd = open("to_server",O_RDONLY);
+   printf("Connected to WKP\n");
 
-  int pvt_fd;
-  pvt_fd = open(msg, O_WRONLY);
+   char pipe_n[HANDSHAKE_BUFFER_SIZE];
+   read(wkp_fd, pipe_n, HANDSHAKE_BUFFER_SIZE);
+   remove("to_server");
 
-    printf("Sending message to client \n" );
+   int pvt_fd = open(pipe_n,O_WRONLY);
+   printf("Sending message to client \n" );
+   write(pvt_fd,ACK,HANDSHAKE_BUFFER_SIZE);
+   printf("sent client acknowledgement message %s\n",ACK);
+   printf("waiting for response from client\n");
 
-    write(pvt_fd,ACK,HANDSHAKE_BUFFER_SIZE);
-  //  printf("server sent client ACK message: %s\n",ACK);
+   char ack_mes[HANDSHAKE_BUFFER_SIZE];
+   read(wkp_fd, ack_mes, HANDSHAKE_BUFFER_SIZE);
+   printf("Response connection is established with client \n" );
+   printf("read message %s\n", ack_mes);
 
-    printf("waiting for response from client\n");
-
-    char ack_mes[HANDSHAKE_BUFFER_SIZE];
-    read(wkp_fd, ack_mes, HANDSHAKE_BUFFER_SIZE);
-  //  printf("read message: %s\n", ack_mes);
-
-    printf("Response connection is established with client \n" );
-    remove("WKP");
-
-    *to_client = pvt_fd;
-    return wkp_fd;
+   *to_client = pvt_fd;
+   return wkp_fd;
 }
 
 
@@ -62,32 +50,27 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int pvt_fd;
-  if ((pvt_fd = mkfifo("PRVP", 0644)) == -1) {
-    printf("Error %d: %s\n", errno, strerror(errno));
-  }
 
-  printf("Trying to connect to well known pipe\n");
-    int wkp_fd;
-    wkp_fd = open("WPK", O_WRONLY);
+    mkfifo("to_client",0644);
 
-  printf("Connected to WKP\n");
+    printf("made private pipe\n");
 
-  printf("Private pipe opened and writing it's file to the well known pipe\n");
+    int wkp_fd = open("to_server",O_WRONLY);
+    char pipe_n[HANDSHAKE_BUFFER_SIZE] = "to_client";
+    write(wkp_fd,pipe_n,HANDSHAKE_BUFFER_SIZE);
+    printf("sent the file name\n");
 
-  pvt_fd = open("PRVP", O_RDONLY);
-
-    printf("Waiting for response from server\n");
+    int pvt_fd = open("to_client",O_RDONLY);
     char ack_mes[HANDSHAKE_BUFFER_SIZE];
-    read(pvt_fd, ack_mes, HANDSHAKE_BUFFER_SIZE);
-
+    read(pvt_fd,ack_mes,HANDSHAKE_BUFFER_SIZE);
     printf("Response recieved from server, sending message\n" );
+    printf("read message %s\n", ack_mes);
+    remove("to_client");
 
     write(wkp_fd, ack_mes, HANDSHAKE_BUFFER_SIZE);
-
     printf("Message sent. Connection established with server\n");
 
-    remove("PRVP");
     *to_server = wkp_fd;
     return pvt_fd;
+
 }
